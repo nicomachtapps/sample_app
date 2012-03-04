9f2d60685d98de1bbccbb75edb9b77c31e8334cd
Nicolas Autzen <nautzen@tzi.de>
2012-03-04 01:43:10 +0100

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
+  resources :users
+  #match 'users/:id', to: 'users#show'
+  #match '/signup', to: 'users#new'
 
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
